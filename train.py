5f22d18e3192944ed846c8213652d1b009380938
Swall0w <technext.jpn@gmail.com>
2017-04-30 17:43:40 +0900

    dataset

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import chainer
-from chainer import cuda, optimizers
+from chainer import cuda, optimizers, datasets
 from model import Generator, Discriminator
 import argparse
 from PIL import Image
 import numpy as np
+import os
 
 def arg():
     parser = argparse.ArgumentParser()
@@ -18,6 +19,7 @@ def arg():
 def main():
     args = arg()
     nz = 100
+    image_path = 'data/'
 
     generator = Generator(nz)
     discriminator = Discriminator()
@@ -38,6 +40,8 @@ def main():
     op_d = optimizers.Adam(alpha=0.0002, beta1=0.5)
     op_d.setup(discriminator)
     op_d.add_hook(chainer.optimizer.WeightDecay(0.00001))
+
+    train = datasets.ImageDataset(os.listdir(image_path))
 
 
 if __name__ == '__main__':
