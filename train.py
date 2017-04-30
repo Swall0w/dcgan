c7062d8bdb5b08b11669a7da36d9efbbc1c1af67
Swall0w <technext.jpn@gmail.com>
2017-04-30 16:54:18 +0900

    modified.

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import chainer
+from chainer import cuda, optimizers
 from model import Generator, Discriminator
 import argparse
 from PIL import Image
+import numpy as np
+
 def arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch','-b',type=int,default=100,help='number of minibatch')
@@ -14,6 +17,28 @@ def arg():
 
 def main():
     args = arg()
+    nz = 100
+
+    generator = Generator(nz)
+    discriminator = Discriminator()
+
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        generator.to_gpu()
+        discriminator.to_gpu()
+        zvis = (cuda.cupy.random.uniform(-1,1,(100,nz), dtype=np.float32))
+    else:
+        zvis = (np.random.uniform(-1,1,(100,nz)))
+        
+
+    op_g = optimizers.Adam(alpha=0.0002, beta1=0.5)
+    op_g.setup(generator)
+    op_g.add_hook(chainer.optimizer.WeightDecay(0.00001))
+
+    op_d = optimizers.Adam(alpha=0.0002, beta1=0.5)
+    op_d.setup(discriminator)
+    op_d.add_hook(chainer.optimizer.WeightDecay(0.00001))
+
 
 if __name__ == '__main__':
     main()
